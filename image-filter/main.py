597bce6bf6e684608b6373d71153639e30d1a5c2
Oliver Arroyo <o.t.arroyo@gmail.com>
2023-05-01 16:30:26 +0100

    Resize images to make them smaller

--- image-filter/main.py
+++ image-filter/main.py
@@ -1,11 +1,19 @@
 from PIL import Image, ImageFilter
 
 # Images need to be converted from 'RGB' to 'RGBA', for Image.blend() to work
-with Image.open("racing-helmet-unsplash.jpg").convert('RGBA') as image_a, Image.open("red-bull-car-unsplash.jpg").convert('RGBA') as image_b:
-	image_b_copy = image_b.copy()
-	# image_b needs to be the same size as image_a, for Image.blend() to work
-	image_b_copy = image_b_copy.resize(image_a.size)
-	for number in range(11):
-		merged = Image.blend(image_a, image_b_copy, number / 10)
-		merged.save(f'merged_{number}.png')
-		#merged.show()
+with Image.open("racing-helmet-unsplash.jpg").convert("RGBA") as image_a, Image.open(
+    "red-bull-car-unsplash.jpg"
+).convert("RGBA") as image_b:
+    image_a_copy = image_a.copy()
+    image_b_copy = image_b.copy()
+    # need to resize image_b to match image_a, for blend() to work
+    image_b_copy = image_b.resize((2265, 2831))
+    # image_b needs to be the same size as image_a, for Image.blend() to work
+    # thumbnail() modifies the size and maintains the aspect ratio
+    image_a_copy.thumbnail((300, 300))
+    image_b_copy.thumbnail((300, 300))
+    
+    for number in range(11):
+        merged = Image.blend(image_a_copy, image_b_copy, number / 10)
+        merged.save(f"merged_{number}.png")
+        # merged.show()
