5aabe2ce99af4813ec878861a911dbcbc1a01692
Oliver Arroyo <o.t.arroyo@gmail.com>
2023-05-01 16:34:14 +0100

    Remove unused class from PIL module

--- image-filter/main.py
+++ image-filter/main.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageFilter
+from PIL import Image
 
 # Images need to be converted from 'RGB' to 'RGBA', for Image.blend() to work
 with Image.open("racing-helmet-unsplash.jpg").convert("RGBA") as image_a, Image.open(
@@ -12,7 +12,7 @@ with Image.open("racing-helmet-unsplash.jpg").convert("RGBA") as image_a, Image.
     # thumbnail() modifies the size and maintains the aspect ratio
     image_a_copy.thumbnail((300, 300))
     image_b_copy.thumbnail((300, 300))
-    
+
     for number in range(11):
         merged = Image.blend(image_a_copy, image_b_copy, number / 10)
         merged.save(f"merged_{number}.png")
